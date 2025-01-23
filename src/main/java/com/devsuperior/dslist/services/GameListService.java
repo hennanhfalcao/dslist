a0723c99d80e369523530e376d180e830ff3c112
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-01-23 18:32:15 -0300

    :sparkles: feat: Customized endpoints for accessing games of a list of games + SQL Query and Projection Interface implemented

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dtos.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> gameLists = gameListRepository.findAll();
+        return gameLists.stream().map(GameListDTO::new).toList();
+    }
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dtos.GameDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,10 +27,14 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> games = gameRepository.findAll();
-       List<GameMinDTO> gamesDto = games.stream().map(GameMinDTO::new).toList();
-       return gamesDto;
+       return games.stream().map(GameMinDTO::new).toList();
     }
 
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> games = gameRepository.searchByList(listId);
+        return games.stream().map(GameMinDTO::new).toList();
 
+    }
 }
