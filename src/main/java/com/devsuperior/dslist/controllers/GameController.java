3dc1c7c70364a2172eb9aedde79174f5cd14996a
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-01-23 17:46:34 -0300

    :sparkles: feat: Get Game by Id endpoint implemented

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dtos.GameDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class GameController {
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> games = gameService.findAll();
         return games;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO game = gameService.findById(id);
+        return game;
     }
 }
